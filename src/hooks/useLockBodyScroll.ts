c8f133e62f3d8f71e9899f9f5201ff40a921a0a1
Conor <conor909@gmail.com>
2019-02-23 19:16:28 +0000

    disable scroll

--- src/hooks/useLockBodyScroll.ts
+++ src/hooks/useLockBodyScroll.ts
@@ -2,7 +2,7 @@ import { useLayoutEffect } from 'react';
 
 export default function useLockBodyScroll () {
   useLayoutEffect(() => {
-    document.body.style.overflow = 'hidden'; // on mount
-    return () => document.body.style.overflow = 'visible'; // Re-enable scrolling when component unmounts
+    document.body.style.overflowY = 'hidden'; // on mount
+    return () => document.body.style.overflowY = 'scroll'; // Re-enable scrolling when component unmounts
    }, []); // Empty array ensures effect is only run on mount and unmount
 }
