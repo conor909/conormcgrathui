0d4f763620c751c0a588c7806a43699764b7cf4b
Conor <conor909@gmail.com>
2019-03-03 18:20:33 +0000

    contact

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -28,97 +28,99 @@ export default (props:any) => {
   }
   
   return(
-    <Formik
-      initialValues={{ email: '', name: '', phone: '', message: '' }}
-      onSubmit={(values) => {
-        setEmailStatus('sending');
-        sendEmail(values);
-      }}
-      validationSchema={
-        Yup.object().shape({
-          name: Yup.string().required('Enter your name'),
-          email: Yup.string().email('Email must be valid').required('Enter your email'),
-          phone: Yup.string(),
-          message: Yup.string().required('Enter a message')
-        })
-      }>
-      {
-        (props) => {
-          const {
-            values,
-            touched,
-            errors,
-            dirty,
-            isSubmitting,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            handleReset,
-          } = props;
-          return(
-            <form onSubmit={ handleSubmit }>
-              <Row justify='center' nogutter={ true }>
-                <FormElements.Input
-                  id='name'
-                  type='text'
-                  label='Name'
-                  value={ values.name }
-                  onChange={ handleChange }
-                  onBlur={ handleBlur }
-                  error={ errors.name ? errors.name : null }
-                />
-              </Row>
-              <Row justify='center' nogutter={ true }>
-                <FormElements.Input
-                  id='email'
-                  label='Email'
-                  type='text'
-                  value={ values.email }
-                  onChange={ handleChange }
-                  onBlur={ handleBlur }
-                  error={ errors.email ? errors.email : null }
-                />
-              </Row>
-              <Row justify='center' nogutter={ true }>
-                <FormElements.Input
-                  id='phone'
-                  label='Phone'
-                  type='text'
-                  value={ values.phone }
-                  onChange={ handleChange }
-                  onBlur={ handleBlur }
-                  error={ errors.phone ? errors.phone : null }
-                />
-              </Row>
-              <Row justify='center' nogutter={ true }>
-                <FormElements.TextArea
-                  id='message'
-                  label='Message'
-                  type='text'
-                  value={ values.message }
-                  onChange={ handleChange }
-                  onBlur={ handleBlur }
-                  error={ errors.message ? errors.message : null }
-                />
-              </Row>
-              <Row justify='center' nogutter={ true }>
-                <div style={{ paddingTop: '1rem', width: '100%' }}>
-                  <FormElements.Button
-                    style={{ width: '100%' }}
-                    type='submit'
-                    onClick={ handleSubmit }
-                    label='Send'
-                    disabled={ isSubmitting || emailStatus === 'sent' ? true : false }
+    <div style={{ maxWidth: '960px', margin: '0 auto' }}>
+      <Formik
+        initialValues={{ email: '', name: '', phone: '', message: '' }}
+        onSubmit={(values) => {
+          setEmailStatus('sending');
+          sendEmail(values);
+        }}
+        validationSchema={
+          Yup.object().shape({
+            name: Yup.string().required('Enter your name'),
+            email: Yup.string().email('Email must be valid').required('Enter your email'),
+            phone: Yup.string(),
+            message: Yup.string().required('Enter a message')
+          })
+        }>
+        {
+          (props) => {
+            const {
+              values,
+              touched,
+              errors,
+              dirty,
+              isSubmitting,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              handleReset,
+            } = props;
+            return(
+              <form onSubmit={ handleSubmit }>
+                <Row justify='center' nogutter={ true }>
+                  <FormElements.Input
+                    id='name'
+                    type='text'
+                    label='Name'
+                    value={ values.name }
+                    onChange={ handleChange }
+                    onBlur={ handleBlur }
+                    error={ errors.name ? errors.name : null }
                   />
-                </div>
-              </Row>
-              <Row justify='center' nogutter={ true }>
-                  { emailStatus === 'sent' && <p style={{ textAlign: 'center' }}>Sent!</p> }
-              </Row>
-          </form>
-          )
+                </Row>
+                <Row justify='center' nogutter={ true }>
+                  <FormElements.Input
+                    id='email'
+                    label='Email'
+                    type='text'
+                    value={ values.email }
+                    onChange={ handleChange }
+                    onBlur={ handleBlur }
+                    error={ errors.email ? errors.email : null }
+                  />
+                </Row>
+                <Row justify='center' nogutter={ true }>
+                  <FormElements.Input
+                    id='phone'
+                    label='Phone'
+                    type='text'
+                    value={ values.phone }
+                    onChange={ handleChange }
+                    onBlur={ handleBlur }
+                    error={ errors.phone ? errors.phone : null }
+                  />
+                </Row>
+                <Row justify='center' nogutter={ true }>
+                  <FormElements.TextArea
+                    id='message'
+                    label='Message'
+                    type='text'
+                    value={ values.message }
+                    onChange={ handleChange }
+                    onBlur={ handleBlur }
+                    error={ errors.message ? errors.message : null }
+                  />
+                </Row>
+                <Row justify='center' nogutter={ true }>
+                  <div style={{ paddingTop: '1rem', width: '100%' }}>
+                    <FormElements.Button
+                      style={{ width: '100%' }}
+                      type='submit'
+                      onClick={ handleSubmit }
+                      label='Send'
+                      disabled={ isSubmitting || emailStatus === 'sent' ? true : false }
+                    />
+                  </div>
+                </Row>
+                <Row justify='center' nogutter={ true }>
+                    { emailStatus === 'sent' && <p style={{ textAlign: 'center' }}>Sent!</p> }
+                </Row>
+            </form>
+            )
+          }
         }
-      }
-    </Formik>
+      </Formik>
+    </div>
   )
 }
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Container } from 'react-awesome-styled-grid';
 import { Page, ContactForm } from '../components';
 
 export default () => {
   return(
-    <Page style={{ background: '#333', width: '100%', color: 'white' }}>
+    <Page style={{ width: '100%' }}>
       <ContactForm />
     </Page>
   );
