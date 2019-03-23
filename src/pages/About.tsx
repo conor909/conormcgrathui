a01b20f6af8fe167ee6614a2a9f8857ebc5af7a9
Conor <conor909@gmail.com>
2019-03-23 12:24:20 +0000

    colors

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -16,17 +16,17 @@ export default (props:any) => (
         color: '#fff',
         background: 'linear-gradient(to left, #84002d, #dc2430)'
       }}>
-      <Col lg={ 2 } md={ 2 } sm={ 12 } xs={ 12 }>
+      <Col lg={ 2 } md={ 12 } sm={ 12 } xs={ 12 }>
         <ScreenClass render={(screen:any) => {
-            const style = (screen === 'sm' || screen === 'xs') ?  { marginLeft: '-15px', marginRight: '-15px', marginTop: '-15px', background: '#ffce00' } : {}
+            const style = (screen !== 'lg') ?  { marginLeft: '-15px', marginRight: '-15px', marginTop: '-15px', background: '#ffce00' } : {}
             return (
-              <div style={ style }>
+              <div style={{ display: 'flex', flexDirection: 'column', ...style }}>
                 <img src={ me } style={{ width: '100%', maxWidth: '200px', margin: '0 auto' }} />
               </div>
             )
           }} />
       </Col>
-      <Col lg={ 10 } md={ 9 } sm={ 12 } xs={ 12 }>
+      <Col lg={ 10 } md={ 12 } sm={ 12 } xs={ 12 }>
         <h1 style={{ marginTop: 0 }}>Dia dhuit, Hey, Hola, Olá, Dzień dobry, नमस्ते, مرحبا, 你好 </h1> 
         <h3 style={{ marginTop: 0 }}>I'm Conor, a JavaScript developer with a focus on high performance apps for fin tech and data analytics companies.</h3>
         <h4 style={{ marginTop: 0 }}>I have 10 years front end experience in web and mobile app development, UI / UX design, data visualisation, wireframes and prototypes.</h4>
@@ -46,9 +46,7 @@ export default (props:any) => (
 const BlurbBox = (props:{ title:string, blurb:string, index:number }) => {
   return (
     <Col lg={ 6 } md={ 12 } sm={ 12 } xs={ 12 }>
-      <ScreenClass render={(screen:any) => (
-        <h3 style={{ color: '#1d1d1d' }}>{ props.title }</h3>
-      )} />
+      <h3 style={{ color: '#1d1d1d' }}>{ props.title }</h3>
       <div style={{ display: 'flex', flex: 1, flexDirection: 'column', background: '#fff', padding: '1rem', border: '1px solid #e1e1e1' }}>
         <p>
           { props.blurb }
