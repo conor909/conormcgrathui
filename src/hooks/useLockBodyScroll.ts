bc0a9b9246f6f0b35e3d3e04272713d93f0dd28a
Conor <conor909@gmail.com>
2019-03-30 12:54:12 +0000

    fix

--- src/hooks/useLockBodyScroll.ts
+++ src/hooks/useLockBodyScroll.ts
@@ -2,16 +2,16 @@ import { useLayoutEffect } from 'react';
 
 export default function useLockBodyScroll () {
   
-  const page:any = document.getElementById('page');
+  //const page:any = document.getElementById('page');
   
   function lockPageAndBody() {
     document.body.style.overflowY = 'hidden';
-    page.style.overflowY  = 'hidden';
+    //page.style.overflowY  = 'hidden';
   }
 
   function unLockPageAndBody() {
     document.body.style.overflowY = 'scroll';
-    page.style.overflowY  = 'inherit';
+    //page.style.overflowY  = 'inherit';
   }
 
   useLayoutEffect(() => {
