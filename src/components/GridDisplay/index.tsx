c70aad9e5e0f159f23996c008cbe66eb487dff5b
Conor <conor909@gmail.com>
2019-07-12 11:20:26 +0100

    ddd

--- src/components/GridDisplay/index.tsx
+++ src/components/GridDisplay/index.tsx
@@ -5,97 +5,98 @@ import { Row, Col } from 'react-awesome-styled-grid';
 import Display from './Display';
 import getWork from '../../data/work';
 
-export default memo(withRouter((props:any) => {
+export default memo(withRouter((props: any) => {
 
-  const itemsView = useRef<any>(null);
-    
-  const [ loadedImageCount, setLoadedImageCount ] = useState<number>(0);
-  const [ selectedItem, setSelectedItem ] = useState<IGridItem | null>(null);
-  const [ items, setItems ] = useState<any>(getItems());
+	const itemsView = useRef<any>(null);
 
-  useEffect(() => {
-    setLoadedImageCount(0);
-    setSelectedItem(null);
-    setItems(getItems());
-  }, [ props.category ]);
+	const [loadedImageCount, setLoadedImageCount] = useState<number>(0);
+	const [selectedItem, setSelectedItem] = useState<IGridItem | null>(null);
+	const [items, setItems] = useState<any>(getItems());
 
-  function getItems() {
-    return getWork().filter(item => (props.category === 'all' || item.category === props.category));
-  }
+	useEffect(() => {
+		setLoadedImageCount(0);
+		setSelectedItem(null);
+		setItems(getItems());
+	}, [props.category]);
 
-  function handleSelectedItem(item:IGridItem) {
-    setSelectedItem(item);
-    props.history.push(`/work/${ props.category }/${ item.niceName}`);
-  }
+	function getItems() {
+		return getWork().filter(item => (props.category === 'all' || item.category === props.category));
+	}
 
-  function onClickBack() {
-    setSelectedItem(null);
-    props.history.goBack();
-  }
+	function handleSelectedItem(item: IGridItem) {
+		setSelectedItem(item);
+		props.history.push(`/work/${props.category}/${item.niceName}`);
+	}
 
-  return (
-    <>
-      {
-        loadedImageCount < items.length && items.map((work:IGridItem) => (
-          <img style={{ display: 'none' }} src={ work.feature } key={ Math.random() } onLoad={ () => setLoadedImageCount(loadedImageCount + 1) } />
-        ))
-      }
-    <Row style={ selectedItem ? { overflow: 'hidden' } : {} } ref={ itemsView }>
-      {
-        loadedImageCount === items.length
-          ? items.map((item:IGridItem, i:number) => <GridItem key={ Math.random() } item={ item } onSelectedItem={ handleSelectedItem } index={ i } location={ props.history.location.pathname } />)
-          : null
-      }
-    </Row>
-      {
-        selectedItem &&
-          <Display
-            onClose={ onClickBack }
-            item={ selectedItem } />
-      }
-    </>
-  )
+	function onClickBack() {
+		setSelectedItem(null);
+		props.history.goBack();
+	}
+
+	return (
+		<>
+			{
+				loadedImageCount < items.length && items.map((work: IGridItem) => (
+					<img style={{ display: 'none' }} src={work.feature} key={Math.random()} onLoad={() => setLoadedImageCount(loadedImageCount + 1)} />
+				))
+			}
+			<Row style={selectedItem ? { overflow: 'hidden' } : {}} ref={itemsView}>
+				{
+					loadedImageCount === items.length
+						? items.map((item: IGridItem, i: number) => <GridItem key={Math.random()} item={item} onSelectedItem={handleSelectedItem} index={i} location={props.history.location.pathname} />)
+						: null
+				}
+			</Row>
+			{
+				selectedItem &&
+				<Display
+					onClose={onClickBack}
+					item={selectedItem} />
+			}
+		</>
+	)
 }));
 
 
-function GridItem (props:{ item:IGridItem, onSelectedItem:(item:IGridItem)=>void, index:number, location:string }) {
-  const itemRef = useRef(null);
-  
-  useEffect(() => {
-    TweenLite.fromTo(itemRef.current, .5, { opacity: 0, y: 20 }, { opacity: 1, y: 0, ease: Power4.easeOut }).delay( props.index / 10 );
-  }, [ props.location ]);
-  
-  return(
-    <Col
-      key={ Math.random() }
-      lg={ 4 } md={ 6 } sm={ 12 } xs={ 12 }>
-        <section ref={ itemRef } onClick={ () => props.onSelectedItem(props.item) } style={{ opacity: 0 }}>
-          <img
-            src={ props.item.feature }
-            style={{
-              width: '100%',
-              height: 'auto',
-              padding: '2rem'
-            }} />
-        </section>
-    </Col>
-  )
+function GridItem(props: { item: IGridItem, onSelectedItem: (item: IGridItem) => void, index: number, location: string }) {
+	const itemRef = useRef(null);
+
+	useEffect(() => {
+		TweenLite.fromTo(itemRef.current, .5, { opacity: 0, y: 20 }, { opacity: 1, y: 0, ease: Power4.easeOut }).delay(props.index / 10);
+	}, [props.location]);
+
+	return (
+		<Col
+			key={Math.random()}
+			lg={4} md={6} sm={12} xs={12}>
+			<section ref={itemRef} onClick={() => props.onSelectedItem(props.item)} style={{ opacity: 0 }}>
+				<img
+					src={props.item.feature}
+					style={{
+						width: '100%',
+						height: 'auto',
+						padding: '2rem'
+					}} />
+			</section>
+		</Col>
+	)
 }
 
 export interface IGridItem {
-  id: number,
-  ref: string;
-  niceName: string;
-  title: string;
-  client: string | null;
-  skills: Array<string>;
-  desc: Array<string>;
-  color: string;
-  fontColor: string;
-  feature: string;
-  images: Array<string>;
-  url: null | string;
-  displayLink: null | string;
-  category: string;
-  gist: null | string;
+	id: number,
+	ref: string;
+	niceName: string;
+	title: string;
+	client: string | null;
+	skills: Array<string>;
+	desc: Array<string>;
+	color: string;
+	fontColor: string;
+	feature: string;
+	images: Array<string>;
+	url: null | string;
+	displayLink: null | string;
+	category: string;
+	gist: null | string;
+	appStores: { ios: string, android: string }
 }
--- src/data/work.ts
+++ src/data/work.ts
@@ -1,6 +1,6 @@
 import kilaBusinessCards from './images/feat_kila_bus_card2.jpg';
 import markland from './images/feat_markland.jpg';
-//import paddysWeekPoster from './images/feat_markland.jpg';
+import ddd from './images/feat_ddd.jpg';
 import peterBlake from './images/feat_peterblake.jpg';
 import liveLounge from './images/feat_rayne.jpg';
 import meGirl from './images/large_megirl1.jpg';
@@ -25,177 +25,238 @@ import orbs from './images/orbs.jpg';
 import rwb from './images/webcomponents.jpg';
 
 export default () => ([
-  {
-    id: 8,
-    ref: 'advisian',
-    niceName: 'advisian',
-    title: 'Advisian Digital',
-    client: 'Advisian',
-    skills: ['Animation', 'React'],
-    desc: ["Advisian Digital Interactive infographic, a single page app with interactive design, animations, icons and graphics."],
-    color: '#005f83',
+	{
+		id: 12,
+		ref: 'ddd',
+		niceName: 'divesitedata',
+		title: 'divesitedata',
+		client: '',
+		skills: ['React', 'React-Native', 'web', 'mobile'],
+		desc: ["divesitedata was built purely for fun. I hope one day it will help promote diving in lesser known regions, and help raise awareness to the importance of preserving our environment.", "It's a web and mobile app built in react & react native and hosted on Heroku."],
+		color: '#005f83',
 		fontColor: '#fff',
-    feature: advisian,
-    images: [ advisian2, advisian ],
-    url: 'https://www.advisian.com/en-gb/what-we-do/services/advisian-digital/digital-future',
-    displayLink: 'www.advisian.com/digital-future',
-    category: 'development',
-    tools: ['react', 'ai']
-  },
-  {
-    id: 7,
-    ref: 'cube19',
-    niceName: 'cube19',
-    title: 'cube19',
-    client: null,
-    skills: ['MVC', 'Data Visualisation', 'UI', 'UX', 'backbone', 'underscore', 'jQuery', 'React'],
-    desc: ["At cube19 I was part of a 3 person front end team.  I was responsible for designing UI, and building complex charting tools and data visualization dashboards.", "As the complexity of the app grew and new front end technology became available (React), we improved the performance and usability of the app by maintaining the older components in backbone / jQuery, while writing new features in React, and eventually refactoring the front end.", "The app is used worldwide by large and small companies, processing large data sets from their CRMs for business analytics and management.", "I also worked on a similar product available on the Salesforce App Exchange and built apps for internal customer management use."],
-    color: '#1e1e1e',
-		fontColor: '#fff',
-    feature: cube19,
-    images: [ cube19_dashboard, cube19_leaderboard, cube19_configure_chart, cube19_4, cube19_1, cube19_3, cube19_2 ],
-    url: 'http://www.cube19.com',
+		feature: ddd,
+		images: [],
+		url: 'https://young-beach-63466.herokuapp.com/',
+		appStores: {
+			android: '',
+			ios: 'https://apps.apple.com/gb/app/divesitedata/id1464499307'
+		},
+		displayLink: 'www.divesitedata.com',
 		category: 'development',
-    tools: ['js', 'react', 'scss', 'backbone']
-  },
-  {
-    id: 10,
-    ref: 'orbs',
-    niceName: 'orbs',
-    title: 'Canvas Sketch',
-    client: null,
-    skills: ['ES6', 'canvas'],
-    desc: [ "Animated orbs using ES6 classes" ],
-    gist: "conor909/d18ea902c31367a672e0a87a6acf663c",
-    category: 'code',
-    feature: orbs,
-    tools: [ 'javascript' ]
-  },
-  {
-    id: 11,
-    ref: 'webcomps',
-    niceName: 'webcomponents',
-    title: 'React App inside a Web Component',
-    client: null,
-    skills: [],
-    desc: [ "Web components are a set of web platform APIs that allow you to create new custom, reusable, encapsulated HTML tags to use in web pages and web apps.", "This is a script I wrote to insert React apps inside custom elements.  There are some webpack build steps to go with it, if your interested in learning more get in touch, or check out the web components community."  ],
-    gist: "conor909/8466594f678d82d7469b2ff70af2e0cb",
-    url: "https://www.webcomponents.org/",
-    displayLink: 'www.webcomponents.org',
-    category: "code",
-    feature: rwb,
-    tools: [ "javascript" ]
-  },
-  {
-    id: 6,
-    ref: 'theWellnessNomad',
-    niceName: 'the-wellness-nomad',
-    title: 'The Wellness Nomad',
-    client: 'The Wellness Nomad',
-    skills: ['Web Development'],
-    desc: ["Web development and database for The Wellness Nomad, Hong Kong."],
-    color: '#61cdcd',
+		tools: ['react', 'react-native', 'ai', 'ps']
+	},
+	{
+		id: 8,
+		ref: 'advisian',
+		niceName: 'advisian',
+		title: 'Advisian Digital',
+		client: 'Advisian',
+		skills: ['Animation', 'React'],
+		desc: ["Advisian Digital Interactive infographic, a single page app with interactive design, animations, icons and graphics."],
+		color: '#005f83',
 		fontColor: '#fff',
-    feature: theWellnessNomad,
-    images: [ theWellnessNomad ],
-    url: 'http://thewellnessnomad.com/',
+		feature: advisian,
+		images: [advisian2, advisian],
+		url: 'https://www.advisian.com/en-gb/what-we-do/services/advisian-digital/digital-future',
+		appStores: {
+			android: '',
+			ios: ''
+		},
+		displayLink: 'www.advisian.com/digital-future',
 		category: 'development',
-    tools: ['php', 'sql', 'html', 'css']
-  },
-  {
-    id: 9,
-    ref: 'planning',
-    niceName: 'planning',
-    title: 'Planning & Documentation',
-    client: null,
-    skills: ['UI', 'UX'],
-    desc: ['Illustration explaining functionality for a complex data storing interface.', 'Depending on each scenario, data needed to be saved to a Redux store or in component state, deleted or overwritten.', "The precedence of the data could vary depending on user interactions. Converstations about this flow weren't easy to communicate, so I came up with this illustration to help conversations during planning, development and testing."],
-    color: '#1e1e1e',
+		tools: ['react', 'ai']
+	},
+	{
+		id: 7,
+		ref: 'cube19',
+		niceName: 'cube19',
+		title: 'cube19',
+		client: null,
+		skills: ['MVC', 'Data Visualisation', 'UI', 'UX', 'backbone', 'underscore', 'jQuery', 'React'],
+		desc: ["At cube19 I was part of a 3 person front end team.  I was responsible for designing UI, and building complex charting tools and data visualization dashboards.", "As the complexity of the app grew and new front end technology became available (React), we improved the performance and usability of the app by maintaining the older components in backbone / jQuery, while writing new features in React, and eventually refactoring the front end.", "The app is used worldwide by large and small companies, processing large data sets from their CRMs for business analytics and management.", "I also worked on a similar product available on the Salesforce App Exchange and built apps for internal customer management use."],
+		color: '#1e1e1e',
 		fontColor: '#fff',
-    feature: tobes,
-    images: [  ],
-    url: '',
+		feature: cube19,
+		images: [cube19_dashboard, cube19_leaderboard, cube19_configure_chart, cube19_4, cube19_1, cube19_3, cube19_2],
+		url: 'http://www.cube19.com',
+		appStores: {
+			android: '',
+			ios: ''
+		},
 		category: 'development',
-    tools: ['react', 'redux', 'ai']
-  },
-  {
-    id: 2,
-    ref: 'marklandInfrastructure',
-    niceName: 'markland',
-    title: 'Markland Infrastructure',
-    client: 'Markland Asia Infrastructure, Hong Kong',
-    skills: ['Web Design', 'Web Development'],
-    desc: ["Corporate branding and full page web design with animated page transitions."],
-    color: '#d5d5d5',
-    fontColor: '#000',
-    feature: markland,
-    images: [ markland ],
-    url: 'http://marklandasia.com/',
+		tools: ['js', 'react', 'scss', 'backbone']
+	},
+	{
+		id: 10,
+		ref: 'orbs',
+		niceName: 'orbs',
+		title: 'Canvas Sketch',
+		client: null,
+		skills: ['ES6', 'canvas'],
+		desc: ["Animated orbs using ES6 classes"],
+		gist: "conor909/d18ea902c31367a672e0a87a6acf663c",
+		category: 'code',
+		feature: orbs,
+		tools: ['javascript']
+	},
+	{
+		id: 11,
+		ref: 'webcomps',
+		niceName: 'webcomponents',
+		title: 'React App inside a Web Component',
+		client: null,
+		skills: [],
+		desc: ["Web components are a set of web platform APIs that allow you to create new custom, reusable, encapsulated HTML tags to use in web pages and web apps.", "This is a script I wrote to insert React apps inside custom elements.  There are some webpack build steps to go with it, if your interested in learning more get in touch, or check out the web components community."],
+		gist: "conor909/8466594f678d82d7469b2ff70af2e0cb",
+		url: "https://www.webcomponents.org/",
+		appStores: {
+			android: '',
+			ios: ''
+		},
+		displayLink: 'www.webcomponents.org',
+		category: "code",
+		feature: rwb,
+		tools: ["javascript"]
+	},
+	{
+		id: 6,
+		ref: 'theWellnessNomad',
+		niceName: 'the-wellness-nomad',
+		title: 'The Wellness Nomad',
+		client: 'The Wellness Nomad',
+		skills: ['Web Development'],
+		desc: ["Web development and database for The Wellness Nomad, Hong Kong."],
+		color: '#61cdcd',
+		fontColor: '#fff',
+		feature: theWellnessNomad,
+		images: [theWellnessNomad],
+		url: 'http://thewellnessnomad.com/',
+		appStores: {
+			android: '',
+			ios: ''
+		},
 		category: 'development',
-    tools: ['js', 'css', 'html']
-  },
-  {
-    id: 3,
-    ref: 'peterBlake',
-    niceName: 'peter-blake',
-    title: 'Peter Blake',
-    client: 'The Cat Street Gallery, Hong Kong',
-    skills: ['Graphic Design', 'Print'],
-    desc: ["Print design for asias largest art fair. An advertisment for the 'Godfather of British Pop' - Peter Blake"],
-    color: '#000d4b',
+		tools: ['php', 'sql', 'html', 'css']
+	},
+	{
+		id: 9,
+		ref: 'planning',
+		niceName: 'planning',
+		title: 'Planning & Documentation',
+		client: null,
+		skills: ['UI', 'UX'],
+		desc: ['Illustration explaining functionality for a complex data storing interface.', 'Depending on each scenario, data needed to be saved to a Redux store or in component state, deleted or overwritten.', "The precedence of the data could vary depending on user interactions. Converstations about this flow weren't easy to communicate, so I came up with this illustration to help conversations during planning, development and testing."],
+		color: '#1e1e1e',
 		fontColor: '#fff',
-    feature: peterBlake,
-    images: [ peterBlake ],
-    url: '',
-		category: 'design',
-    tools: ['ps', 'ai']
-  },
-  {
-    id: 1,
-    ref: 'kilaBusinessCards',
-    niceName: 'kila',
-    title: 'Kila',
-    client: 'Kila Bar',
-    skills: [ 'Graphic Design', 'Print' ],
-    desc: ["Business cards for Kila bar, Hong Kong."],
-    color: '#1e211a',
+		feature: tobes,
+		images: [],
+		url: '',
+		appStores: {
+			android: '',
+			ios: ''
+		},
+		category: 'development',
+		tools: ['react', 'redux', 'ai']
+	},
+	{
+		id: 2,
+		ref: 'marklandInfrastructure',
+		niceName: 'markland',
+		title: 'Markland Infrastructure',
+		client: 'Markland Asia Infrastructure, Hong Kong',
+		skills: ['Web Design', 'Web Development'],
+		desc: ["Corporate branding and full page web design with animated page transitions."],
+		color: '#d5d5d5',
+		fontColor: '#000',
+		feature: markland,
+		images: [markland],
+		url: 'http://marklandasia.com/',
+		appStores: {
+			android: '',
+			ios: ''
+		},
+		category: 'development',
+		tools: ['js', 'css', 'html']
+	},
+	{
+		id: 3,
+		ref: 'peterBlake',
+		niceName: 'peter-blake',
+		title: 'Peter Blake',
+		client: 'The Cat Street Gallery, Hong Kong',
+		skills: ['Graphic Design', 'Print'],
+		desc: ["Print design for asias largest art fair. An advertisment for the 'Godfather of British Pop' - Peter Blake"],
+		color: '#000d4b',
 		fontColor: '#fff',
-    feature: kilaBusinessCards,
-    images: [ kilaBusinessCards ],
-    url: '',
+		feature: peterBlake,
+		images: [peterBlake],
+		url: '',
+		appStores: {
+			android: '',
+			ios: ''
+		},
 		category: 'design',
-    tools: ['ps', 'ai']
-  },
-  {
-    id: 4,
-    ref: 'rayne',
-    niceName: 'rayne',
-    title: 'Live Lounge',
-    client: 'Rayne',
-    skills: ['Graphic Design', 'Print'],
-    desc: ["Print design for Rayne's Live Lounge, Hong Kong"],
-    color: '#2b0027',
+		tools: ['ps', 'ai']
+	},
+	{
+		id: 1,
+		ref: 'kilaBusinessCards',
+		niceName: 'kila',
+		title: 'Kila',
+		client: 'Kila Bar',
+		skills: ['Graphic Design', 'Print'],
+		desc: ["Business cards for Kila bar, Hong Kong."],
+		color: '#1e211a',
 		fontColor: '#fff',
-    feature: liveLounge,
-    images: [ liveLounge ],
-    url: '',
+		feature: kilaBusinessCards,
+		images: [kilaBusinessCards],
+		url: '',
+		appStores: {
+			android: '',
+			ios: ''
+		},
 		category: 'design',
-    tools: ['ps', 'ai']
-  },
-  {
-    id: 5,
-    ref: 'meGirl',
-    niceName: 'me-girl',
-    title: 'Style Me Girl',
-    client: 'Frenzoo, Hong Kong',
-    skills: ['UI', 'UX', 'Illustration', 'Web Design', 'Web Development'],
-    desc: ["I designed the branding and game mechanics, as well as the UI & UX and graphics for Frenzoo’s first mobile game.", "It gained them noticeable presence in the gaming industry and received over 1 million downloads and became the No.1 RPG on the Apple App Store in the U.S., No.1 app on the worldwide Amazon market and now has over 2 million downloads worldwide."],
-    color: '#750047',
-    fontColor: '#fff',
-    feature: meGirl,
-    images: [ meGirl ],
-    url: '',
+		tools: ['ps', 'ai']
+	},
+	{
+		id: 4,
+		ref: 'rayne',
+		niceName: 'rayne',
+		title: 'Live Lounge',
+		client: 'Rayne',
+		skills: ['Graphic Design', 'Print'],
+		desc: ["Print design for Rayne's Live Lounge, Hong Kong"],
+		color: '#2b0027',
+		fontColor: '#fff',
+		feature: liveLounge,
+		images: [liveLounge],
+		url: '',
+		appStores: {
+			android: '',
+			ios: ''
+		},
 		category: 'design',
-    tools: ['ps', 'ai', 'js', 'css', 'html']
-  }
+		tools: ['ps', 'ai']
+	},
+	{
+		id: 5,
+		ref: 'meGirl',
+		niceName: 'me-girl',
+		title: 'Style Me Girl',
+		client: 'Frenzoo, Hong Kong',
+		skills: ['UI', 'UX', 'Illustration', 'Web Design', 'Web Development'],
+		desc: ["I designed the branding and game mechanics, as well as the UI & UX and graphics for Frenzoo’s first mobile game.", "It gained them noticeable presence in the gaming industry and received over 1 million downloads and became the No.1 RPG on the Apple App Store in the U.S., No.1 app on the worldwide Amazon market and now has over 2 million downloads worldwide."],
+		color: '#750047',
+		fontColor: '#fff',
+		feature: meGirl,
+		images: [meGirl],
+		url: '',
+		appStores: {
+			android: '',
+			ios: ''
+		},
+		category: 'design',
+		tools: ['ps', 'ai', 'js', 'css', 'html']
+	}
 ]);
