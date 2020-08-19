6b885fd9c7778732bbb1b44bf9f1b0331c3e4583
Conor <conor909@gmail.com>
2020-08-19 22:37:20 +0100

    updated cors policy

--- server.js
+++ server.js
@@ -6,14 +6,13 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-let whitelist = ['http://conor-ui.com/*', 'https://conor-ui.com/*', 'https://biasharamarketing.co.tz/*', 'http://biasharamarketing.co.tz/*']
+let whitelist = ['http://conor-ui.com/*', 'https://conor-ui.com/*']
 app.use(cors({
   origin: function(origin, callback){
     // allow requests with no origin
     if(!origin) return callback(null, true);
     if(whitelist.indexOf(origin) === -1){
-      var message = 'The CORS policy for this origin doesn't ' +
-                'allow access from the particular origin.';
+      var message = "The CORS policy for this origin doesn't allow access from the particular origin.";
       return callback(new Error(message), false);
     }
     return callback(null, true);
